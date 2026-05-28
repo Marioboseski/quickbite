a5886cd4db52526f501520ee52e5f5924062eb8a
Mario Boseski <crboseskimario@gmail.com>
2026-05-28 16:55:50 +0200

    Add basic styles in Cart.tsx

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -4,13 +4,13 @@ const Cart = () => {
   const { cartItems } = useCart();
 
   return (
-    <div> 
+    <div className="flex flex-col justify-center items-center gap-3"> 
       <h2>Cart</h2>
 
       {cartItems.map((item) => (
-        <div key={item.id}>
+        <div key={item.id} className="flex justify-between items-center p-1 min-h-24 w-full max-w-sm border rounded-md">
           <p>{item.title}</p>
-          <p>{item.price}</p>
+          <p>${item.price}</p>
           <p>Quantity: {item.quantity}</p>
         </div>
       ))}
