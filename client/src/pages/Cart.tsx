504310f5d26da1168dcff662887082e13dfc4836
Mario Boseski <crboseskimario@gmail.com>
2026-05-29 02:16:41 +0200

    Add initial cart styles

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -4,16 +4,20 @@ const Cart = () => {
   const { cartItems, addToCart, decreaseQuantity } = useCart();
 
   return (
-    <div className="flex flex-col justify-center items-center gap-3">
-      <h2>Cart</h2>
+    <div className="flex flex-col items-center gap-3">
+      <h2 className="text-3xl text-amber-400">Cart</h2>
 
       {cartItems.map((item) => (
-        <div key={item.id} className="flex justify-between items-center p-1 min-h-24 w-full max-w-sm border rounded-md">
-          <p>{item.title}</p>
-          <p>${item.price}</p>
-          <button onClick={() => decreaseQuantity(item.id)}>-</button>
-          <button onClick={() => addToCart(item)}>+</button>
-          <p>Quantity: {item.quantity}</p>
+        <div key={item.id} className="flex justify-between items-center text-center p-1 min-h-24 w-full max-w-sm border rounded-md">
+          <div>
+            <p>{item.title}</p>
+            <p>${item.price}</p>
+          </div>
+          <div className="flex justify-around items-center p-2 w-full max-w-24">
+            <button onClick={() => decreaseQuantity(item.id)} className="border w-1/2 p-1">-</button>
+             <p className="border w-1/2 p-1">{item.quantity}</p>
+            <button onClick={() => addToCart(item)} className="border w-1/2 p-1">+</button>
+          </div>
         </div>
       ))}
     </div>
