179d84f475274ffec7f0d531265569bae73af384
Mario Boseski <crboseskimario@gmail.com>
2026-06-02 14:11:01 +0200

    Add backend order valdation

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,5 +1,31 @@
 export const checkout = (req, res) => {
 
+  const { fullName, phoneNumber, address, items, totalPriceeee } = req.body;
+
+  if (!fullName) {
+    return res.status(400).json({
+      message: "Full name is required"
+    });
+  }
+
+  if (!phoneNumber) {
+    return res.status(400).json({
+      message: "Phone number is requred"
+    });
+  }
+
+  if (!address) {
+    return res.status(400).json({
+      message: "Address is required"
+    })
+  }
+
+  if (!items || items.length === 0) {
+    return res.status(400).json({
+      message: "Cart is empty"
+    })
+  }
+
   res.status(200).json({
     message: "Order received"
   });
