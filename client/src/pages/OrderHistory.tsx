87e406c5c947479404eb3026312bc449a817e670
Mario Boseski <crboseskimario@gmail.com>
2026-06-09 19:01:15 +0200

    Improve styles in OrderHistory.tsx

--- client/src/pages/OrderHistory.tsx
+++ client/src/pages/OrderHistory.tsx
@@ -20,21 +20,22 @@ const HistoryOrders = () => {
 
 
   return (
-    <div>
-      <p>History orders</p>
+    <div className="flex flex-col justify-center gap-3 p-2">
+      <h3 className="text-2xl text-center">History orders</h3>
       {orders.map((order) => (
-        <div key={order.id}>
-          <p>{order.fullName}</p>
-          <p>{order.phoneNumber}</p>
-          <p>{order.address}</p>
-          <p>{order.paymentMethod}</p>
+        <div key={order.id} className="border-b p-1">
+          <p>Name: {order.fullName}</p>
+          <p>Phone Number: {order.phoneNumber}</p>
+          <p>Address: {order.address}</p>
           {(order.items).map((orderItem) => (
             <div key={orderItem.id}>
-              <p>{orderItem.title}</p>
+              <p>Product: {orderItem.title}</p>
               <p>Quantity: {orderItem.quantity}</p>
+              <p>Price: ${orderItem.price * orderItem.quantity}</p>
             </div>
           ))}
-          <p>{new Date(order.createdAt).toLocaleString()}</p>
+          <p>Payment method: {order.paymentMethod}</p>
+          <p>Ordered: {new Date(order.createdAt).toLocaleString()}</p>
         </div>
       ))}
     </div>
